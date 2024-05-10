a780faee05831dd8a8f05b4c47e5626b522b58a7
PRI2206 <pr63547890@gecg28.ac.in>
2024-05-10 18:13:15 +0530

    make submit visible

--- views/utils/response_utils.py
+++ views/utils/response_utils.py
@@ -6,9 +6,15 @@ from .image_utils import image_to_nparry
 def response_from_prediction(pred_arr,classes,files_name):
     return [ {
         "file_name":files_name[i],
-        "class":classes[np.argmax(pred)],
-        "confidence":float(pred.max())
-        } for i,pred in enumerate(pred_arr)]
+        "class":classes[np.argmax(preds)],
+        "confidence":round(float(preds.max()),2),
+        "all":[
+            {
+                "class":classes[j],
+                "confidence":round(float(pred)*100,2)
+            }for j,pred in enumerate(preds)
+        ]
+        } for i,preds in enumerate(pred_arr)]
 
 
 async def files_to_np_array(files):
